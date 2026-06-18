645386e3fca2e06d4831b67bcf044dbc550a6929
Maruhti Pratap Adpula <maruthiprathap66@gmail.com>
2026-06-18 13:45:50 +0530

    Time: 0 ms (100%), Space: 42.9 MB (77.39%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+        /*int l=0,r=piles.length-1;
+        int alice=0,bob=0;
+        while(l<r){
+            if(piles[l]<piles[r]){
+                alice=piles[r];
+                bob=piles[l];
+            }else{
+                alice=piles[l];
+                bob=piles[r];
+            }
+            l++;
+            r--;
+        }*/
+    }
+}
